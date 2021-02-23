5965084bb9c61b102749875f03e1db9d6897e596
tim108108 <tim108108@gmail.com>
2021-02-24 00:16:02 +0800

    2.0

--- js/it.js
+++ js/it.js
@@ -12,25 +12,20 @@ function ShowTime(){
     var s=NowDate.getSeconds();　
     document.getElementById('nowtime').innerHTML = '<br>目前時間&nbsp'+h+'時'+mi+'分'+s+'秒';
     document.getElementById('nowdate').innerHTML = '<br>西元&nbsp'+y+'年'+mo+'月'+d+'日';
-    setInterval('ShowTime()');
+    setTimeout('ShowTime()');
     }
+var sel
 function processFormData() {
-  const form = document.forms['updat'];
-  const should = form.elements.should.value;
-  const real = form.elements.real.value;
-  var sel = form.elements.sel.value;
-  document.getElementById('should').innerHTML = should;
-  document.getElementById('real').innerHTML = real;
-
-  sels = (sel*60*60-1);
-  selm = parseInt(sels/60);
-  selh = parseInt(selm/60);
-  sels = (sel*60*60-1)%60%60;
-
-  document.getElementById('sel').innerHTML = selh+'時'+selm+'分'+sels+'秒';
-
-  //setInterval('alertFunc()');
-  //alert("sel" + sel );
-  //alert("should " + should + "\nreal " + real);
-}
-
+    const form = document.forms['updat'];
+    const should = form.elements.should.value;
+    const real = form.elements.real.value;
+//============================================================
+    sel =form.elements.sel.value;
+    document.getElementById('should').innerHTML = should;
+    document.getElementById('real').innerHTML = real;
+//=====================loop here==============================
+    document.getElementById('sel').innerHTML = sel;
+//  alert("should " + should + "real " + real);
+//  alert(sel)
+    }
+//=============================================================
